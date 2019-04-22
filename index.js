ebcffccae54696c24da1b61d0454252e704ffba6
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-04-22 12:35:33 +0200

    tested and completed

--- index.js
+++ index.js
@@ -1,56 +1,49 @@
-import { readFileSync } from 'fs';
+const fs = require('fs');
 
-var array = readFileSync('input.txt').toString().split("\n");
+const inputData = fs.readFileSync('input.txt').toString().split("\n");
 
-const room = {x: parseInt(array[0].split(" ")[0]), y: parseInt(array[0].split(" ")[1])};
-const robot = {x: parseInt(array[1].split(" ")[0]), y: parseInt(array[1].split(" ")[1])};
+const room = {x: parseInt(inputData[0].split(" ")[0]), y: parseInt(inputData[0].split(" ")[1])};
+const robot = {x: parseInt(inputData[1].split(" ")[0]), y: parseInt(inputData[1].split(" ")[1])};
+const directions = inputData[inputData.length -1];
+let  dirtPatchesCleaned = 0
 
-const dirtPatch = {x:0, y: 0};
-
-const dirts =  array.slice(2,array.length-1).forEach( dirt => {
-    const eachDirt = dirt.split(" ");
+const dirtPatches = inputData.slice(2,inputData.length-1).map( dirt => {
+    let eachDirt = dirt.split(" ");
+    let dirtPatch = {x:0, y:0};
     dirtPatch.x = parseInt(eachDirt[0]);
     dirtPatch.y = parseInt(eachDirt[1]);
-    console.log(dirtPatch);
+    return dirtPatch;
 });
 
-const directions = array[array.length -1];
-let  dirtPatchesCleaned = 0
-console.log(dirts);
-
-function moveRobot(input) {
-    input.split("").forEach(letter => {
-         switch(letter) {
-             case "N":
-              robot.y != room.x ? robot.y += 1 : robot.y;
-             break;
-             case "E":
-            // robot.x += 1;
-             robot.x != room.y  ? robot.x += 1 : robot.x;
- 
-             break;
-             case "S":
-            // robot.y -= 1;
+const moveRobot = directions => {
+    directions.split("").forEach(direction => {
+        switch(direction) {
+            case "N":
+            robot.y != room.x ? robot.y += 1 : robot.y;
+            break;
+            case "E":
+            robot.x != room.y  ? robot.x += 1 : robot.x;
+            break;
+            case "S":
             robot.y != room.x ? robot.y -= 1 : robot.y;
-             break;
-             case "W":
-             //robot.x -= 1;
-             robot.x != room.y  ? robot.x -= 1 : robot.x;
- 
-             break;
-         }    
-         cleanDirt();
-     });
- }
+            break;
+            case "W": 
+            robot.x != room.y  ? robot.x -= 1 : robot.x;
+            break;
+        }    
+        cleanDirt(dirtPatches); 
+    });
+};
 
- function cleanDirt() {
-    if(robot.x === dirtPatch.x && robot.y === dirtPatch.y) {
-        dirtPatchesCleaned++;
-        dirtPatch.x = 0;
-        dirtPatch.y = 0;     
-    }   
-    //console.log(robot.x, robot.y);
-}
+const cleanDirt = dirtPatches => {
+    dirtPatches.forEach(dirtPatch => {
+        if(robot.x === dirtPatch.x && robot.y === dirtPatch.y) {
+            dirtPatchesCleaned++; 
+            dirtPatch.x = null;
+            dirtPatch.y = null;     
+        }  
+    });   
+};
 
 moveRobot(directions);
 console.log(robot);
